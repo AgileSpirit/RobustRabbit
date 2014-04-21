ffd07841a2ccc46cc15b29ce982c4ea2b34e4300
Jérémy Buget <jbuget@agile-spirit.fr>
2014-04-21 23:22:22 +0200

    Change Logger

--- src/main/java/io/jrocket/robustrabbit/Consumer.java
+++ src/main/java/io/jrocket/robustrabbit/Consumer.java
@@ -4,6 +4,8 @@ import com.rabbitmq.client.Channel;
 import com.rabbitmq.client.Connection;
 import com.rabbitmq.client.ConnectionFactory;
 import com.rabbitmq.client.QueueingConsumer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * User: OCTO-JBU
@@ -11,6 +13,9 @@ import com.rabbitmq.client.QueueingConsumer;
  * Time: 00:57
  */
 public class Consumer {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(Consumer.class);
+
     private final static String QUEUE_NAME = "RobustRabbit";
 
     public static void main(String[] argv) throws java.io.IOException, java.lang.InterruptedException {
@@ -23,7 +28,7 @@ public class Consumer {
         Channel channel = connection.createChannel();
         channel.queueDeclare(QUEUE_NAME, false, false, false, null);
 
-        System.out.println(" [*] Waiting for messages. To exit press CTRL+C");
+        LOGGER.info(" [*] Waiting for messages. To exit press CTRL+C");
 
         // Declare a new QueueingConsumer and assign it to the channel
         QueueingConsumer consumer = new QueueingConsumer(channel);
@@ -32,7 +37,7 @@ public class Consumer {
         while (true) {
             QueueingConsumer.Delivery delivery = consumer.nextDelivery();
             String message = new String(delivery.getBody());
-            System.out.println(" [x] Received '" + message + "'");
+            LOGGER.info(" [x] Received '" + message + "'");
         }
     }
 }
